b9fb3e6206d779dfb0b0f8413c7bf397e521b2b7
khaled <khaledboussaba@gmail.com>
2019-07-11 14:42:33 +0200

    CRUD Fournisseur OK

---
+++ src/main/java/com/stock/mvc/controllers/FournisseurController.java
@@ -0,0 +1,78 @@
+package com.stock.mvc.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.stock.mvc.entities.Fournisseur;
+import com.stock.mvc.services.IFournisseurService;
+
+@Controller
+@RequestMapping(value ="/fournisseur")
+public class FournisseurController {
+
+	@Autowired
+	private IFournisseurService fournisseurService;
+	
+	@RequestMapping(value  = "/")
+	public String fournisseur(Model model) {
+		List<Fournisseur> fournisseurs = fournisseurService.selectAll();
+		if (fournisseurs == null) {
+			fournisseurs = new ArrayList<Fournisseur>();
+		}
+		model.addAttribute("fournisseurs", fournisseurs);
+		return "fournisseur/fournisseur";
+	}
+	
+	
+	@RequestMapping(value = "/nouveau", method = RequestMethod.GET)
+	public String ajouterFournisseur(Model model) {
+		Fournisseur fournisseur = new Fournisseur();
+		model.addAttribute(fournisseur);
+		return "fournisseur/ajouterFournisseur";
+	}
+
+	@RequestMapping(value = "/enregistrer", method = RequestMethod.POST)
+	public String enregistrerFournisseur(Model model, Fournisseur fournisseur) {
+
+		fournisseur.setPhoto("f1.png");
+		
+		if (fournisseur.getIdFournisseur() != null) {
+			fournisseurService.update(fournisseur);
+		} else {
+			fournisseurService.save(fournisseur);
+		}
+		return "redirect:/fournisseur/";
+	}
+	
+	@RequestMapping(value = "/modifier/{idFournisseur}")
+	public String modifierClient(Model model, @PathVariable Long idFournisseur) {
+		if (idFournisseur != null) {
+			Fournisseur fournisseur = fournisseurService.getById(idFournisseur);
+			if (fournisseur != null) {
+				model.addAttribute(fournisseur);
+			}
+		}
+		return "fournisseur/ajouterFournisseur";
+	}
+	
+	@RequestMapping(value = "/supprimer/{idFournisseur}")
+	public String supprimerClient(Model model, @PathVariable Long idFournisseur) {
+		if (idFournisseur != null) {
+			Fournisseur fournisseur = fournisseurService.getById(idFournisseur);
+			if(fournisseur != null) {
+				//TODO verification avant suppression
+				fournisseurService.remove(idFournisseur);
+			}
+		}
+		return "redirect:/fournisseur/";
+	}
+	
+	
+}
